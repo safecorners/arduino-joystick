7c069786c02deb9fafb380bcd1990712b9c79935
safecorners <safecorners.mobile@gmail.com>
2024-06-10 09:37:27 +0900

    use `string` to build a message

--- joystick.ino
+++ joystick.ino
@@ -1,30 +1,32 @@
-const int VRX_PIN = A0 ;
-const int VRY_PIN = A1 ;
-const int SW_PIN = A2 ;
+const int VRX_PIN = A0;
+const int VRY_PIN = A1;
+const int SW_PIN = A2;
 
-void setup() {
-
+void setup()
+{
     pinMode(SW_PIN, INPUT_PULLUP);
-    
     Serial.begin(9600);
-    Serial.println("Joystick Ready. format: (VRX, VRY, SW)");
 }
 
-void loop() {
+void loop()
+{
     int VRX = analogRead(VRX_PIN);
     int VRY = analogRead(VRY_PIN);
     int SW = digitalRead(SW_PIN);
-    if (Serial.available()) {
+
+    if (Serial.available())
+    {
         char command = Serial.read();
-        if (command == 's') {
+        if (command == 's')
+        {
             sendSensingValues(VRX, VRY, SW);
         }
     }
 }
 
-void sendSensingValues(int VRX, int VRY, int SW) {
-    char buffer[50];
-    sprintf(buffer, "(%d, %d, %d)", VRX, VRY, SW);
-    Serial.println(buffer);
+void sendSensingValues(int VRX, int VRY, int SW)
+{
+    String message = "(" + String(VRX) + ", " + String(VRY) + ", " + String(SW) + ")\n";
+    Serial.print(message);
     Serial.flush();
 }
